508340c24b07dd058a0e276b1313f7cd8b6fc721
Eric x. sun <followyourheart1211@gmail.com>
2013-09-30 21:02:29 +0800

    test for None return, stop

--- src/test_thread_pool.py
+++ src/test_thread_pool.py
@@ -1,21 +1,28 @@
 # encoding: utf-8
 
-from src.thread_pool import WorkerManager
-import sys
+from thread_pool import WorkerManager
+# import sys
 import time
 
 def do_job(msg):
-    sys.stdout.write(msg)
-    time.sleep(5)
+#     sys.stdout.write(msg)
+#     print 'in do job:', msg
+    return msg
     
     
 if __name__ == '__main__':
     st = time.time()
     wm = WorkerManager(5, 5)
-    for i in range(1000):
-        msg = 'start:%s, sleep 5sec\n' %i
-        wm.add_job(do_job, msg)
+    
+    wm.add_job(do_job, None)
+    
+    for i in range(1, 100):
+        wm.add_job(do_job, i)
+    
     wm.wait_all_complete()
+    res = wm.get_result()
+    wm.stop()
+    print 'res:', res
     ed = time.time()
     
     print 'cost time: %s' %(ed - st)
