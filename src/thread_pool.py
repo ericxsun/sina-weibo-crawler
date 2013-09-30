4abb19b07bcdae969b65859c9ff31e12a94c9cd9
Eric x. sun <followyourheart1211@gmail.com>
2013-09-30 21:37:58 +0800

    get return, stop all workers if one returns None value

--- src/thread_pool.py
+++ src/thread_pool.py
@@ -5,23 +5,32 @@ import sys
 import threading
 
 class Worker(threading.Thread):
-    def __init__(self, work_queue, timeout=1, **kwargs):
+    def __init__(self, worker_manager, **kwargs):
         threading.Thread.__init__(self, **kwargs)
         self.setDaemon(True)
         
-        self.work_queue = work_queue
-        self.timeout    = timeout
+        self.worker_manager = worker_manager
+        self.timeout = worker_manager.timeout
+        self.state = None
         
         self.start()
     
     def run(self):
         while True:
+            if self.state == 'STOP':
+                break
+            
             try:
-                func, args, kwargs = self.work_queue.get(timeout=self.timeout)
+                func, args, kwargs = self.worker_manager.work_queue.get(timeout=self.timeout)
                 
-                func(*args, **kwargs)
+                res = func(*args, **kwargs)
+
+                if res is None:
+                    self.worker_manager.res_is_None = True
+                    self.state = 'STOP'
+                    self.worker_manager.stop()
                 
-                self.work_queue.task_done()
+                self.worker_manager.work_queue.task_done()
             except Queue.Empty:
                 break
             except Exception, e:
@@ -29,16 +38,21 @@ class Worker(threading.Thread):
                 print sys.exc_info()[:2]
                 break
 
+    def stop(self):
+        self.state = 'STOP'
+
 class WorkerManager(object):
     def __init__(self, num_workers=5, timeout=1):
         self.work_queue = Queue.Queue()
+        self.res_is_None = False
+        
         self.workers = []
         self.timeout = timeout
         self.recruit_threads(num_workers)
         
     def recruit_threads(self, num_workers):
         for _ in range(num_workers):
-            worker = Worker(self.work_queue, self.timeout)
+            worker = Worker(self)
             self.workers.append(worker)
     
     def add_job(self, func, *args, **kwargs):
@@ -48,3 +62,15 @@ class WorkerManager(object):
         for worker in self.workers:
             if worker.isAlive():
                 worker.join()
+
+    def work_done(self):
+        self.work_queue.task_done()
+        
+    def get_result(self, *args, **kwargs):
+        return self.res_is_None
+    
+    def stop(self):
+        for worker in self.workers:
+            worker.stop()
+            
+        del self.workers[:]
