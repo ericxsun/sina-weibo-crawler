89c9c473ec1191aad016de5456b4c56f6d2e235b
Eric x. sun <followyourheart1211@gmail.com>
2013-09-30 21:54:33 +0800

    not important

--- src/test_fetcher.py
+++ src/test_fetcher.py
@@ -11,14 +11,15 @@ msg_id  = '10036505028'
 msg_url = 'http://weibo.com/1657470871/A0TpPBtt3'
 
 page    = 1  
-# uid = 1039646267
+uid = 1039646267
 # uid = 3079645245
 # uid = 1043325954
 # uid = 1806128454
 # uid = 1002697421
 # uid = 3087118795
-uid = 3045056321
+# uid = 3045056321
 # uid = 3104811705
+# uid = 2901331743
 page = 1
 
 f_weibos  = './test/weibos-%s.txt'   %(uid)
@@ -40,35 +41,35 @@ print fetcher.check_user(uid)
  
 print 'test for check message exist...'
 print fetcher.check_message(msg_url)
-
+ 
 print 'test for fetch weibo...'
 html = fetcher.fetch_weibo(uid=uid, page=page)
 with codecs.open(f_weibos, 'w', 'utf-8') as f:
     f.write(html)
-
+ 
 print 'test for fetch follows...'
 url = 'http://weibo.com/%s/follow?page=%s' % (uid, page)
 html = fetcher.fetch(url, settings.QUERY_FOLLOWS)
 with codecs.open(f_follows, 'w', 'utf-8') as f:
     f.write(html)
- 
+  
 print 'test for fetch fans...'
 url = 'http://weibo.com/%s/fans?page=%s' % (uid, page)
 html = fetcher.fetch(url, settings.QUERY_FANS)
 with codecs.open(f_fans, 'w', 'utf-8') as f:
     f.write(html)
-     
+      
 print 'test for fetch infos...'
 url = 'http://weibo.com/%s/info' % uid
 html = fetcher.fetch(url, settings.QUERY_INFO)
 with codecs.open(f_infos, 'w', 'utf-8') as f:
     f.write(html)
-                    
+                     
 print 'test for fetch message repost'
 html, page_cnt = fetcher.fetch_msg_reposts(msg_id, page)
 with codecs.open(f_reposts, 'w', 'utf-8') as f:
     f.write(html)
-                   
+                    
 print 'test for fetch message comment'
 html, page_cnt = fetcher.fetch_msg_comments(msg_id, page)
 with codecs.open(f_comments, 'w', 'utf-8') as f:
